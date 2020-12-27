dd0db8865f9103b7fa7cc8f182f9669c711e87e9
Ali Sadeghian <aliosdn@gmaill.com>
2020-12-27 13:17:43 +0330

    Add mainActivity tag to manifest

---
+++ presentation/src/main/kotlin/com/alisdnn/userpanel/presentation/user/login/LoginViewModel.kt
@@ -0,0 +1,15 @@
+package com.alisdnn.userpanel.presentation.user.login
+
+import androidx.hilt.lifecycle.ViewModelInject
+import com.alisdnn.userpanel.presentation.base.viewmodel.BaseViewModel
+
+class LoginViewModel
+@ViewModelInject
+constructor(
+) : BaseViewModel() {
+
+    fun checkUserValidity(username: String, password: String) {
+
+    }
+
+}
