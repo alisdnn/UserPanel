e02bea759114592202d910381934888aac67d4ac
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-01 21:18:02 +0330

    Add SignupFragment.kt

---
+++ presentation/src/main/kotlin/com/alisdnn/userpanel/presentation/signup/SignupFragment.kt
@@ -0,0 +1,10 @@
+package com.alisdnn.userpanel.presentation.signup
+
+import androidx.fragment.app.Fragment
+import com.alisdnn.userpanel.presentation.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignupFragment : Fragment(R.layout.fragment_signup) {
+
+}
