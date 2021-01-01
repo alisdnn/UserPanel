823b9c093cea46b0d9225130d51157c2ae7edd81
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-01 20:12:42 +0330

    Add LoginViewModel.kt

---
+++ presentation/src/main/kotlin/com/alisdnn/userpanel/presentation/login/LoginViewModel.kt
@@ -0,0 +1,11 @@
+package com.alisdnn.userpanel.presentation.login
+
+import androidx.hilt.lifecycle.ViewModelInject
+import com.alisdnn.userpanel.presentation.base.viewmodel.BaseViewModel
+
+class LoginViewModel
+@ViewModelInject
+constructor(
+) : BaseViewModel() {
+
+}
