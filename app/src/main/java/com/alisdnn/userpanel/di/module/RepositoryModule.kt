bf8f1fb43ec384e8d44b2f40f9069e127c293f99
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-01 20:27:34 +0330

    Add fun provideUserDao

--- app/src/main/java/com/alisdnn/userpanel/di/module/RepositoryModule.kt
+++ app/src/main/java/com/alisdnn/userpanel/di/module/RepositoryModule.kt
@@ -1,12 +1,23 @@
-package com.alisdnn.userpanel.mvvmtemplate.di.module
+package com.alisdnn.userpanel.di.module
 
+import com.alisdnn.userpanel.data.db.base.UserDatabase
+import com.alisdnn.userpanel.data.db.dao.UserDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
 
 @InstallIn(ApplicationComponent::class)
 @Module
 class RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideUserDao(
+        userDatabase: UserDatabase
+    ): UserDao {
+        return userDatabase.userDao()
+    }
 
 }
