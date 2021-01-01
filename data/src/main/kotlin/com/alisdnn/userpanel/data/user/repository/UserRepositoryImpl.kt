53852dd03af4dec7ad439005cd62825e9ee17ed5
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-01 20:20:28 +0330

    Add UserRepositoryImpl.kt and override fun getUser

---
+++ data/src/main/kotlin/com/alisdnn/userpanel/data/user/repository/UserRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.alisdnn.userpanel.data.user.repository
+
+import com.alisdnn.userpanel.data.common.extension.applyIoScheduler
+import com.alisdnn.userpanel.data.user.datasource.UserDataSource
+import com.alisdnn.userpanel.domain.user.entity.User
+import com.alisdnn.userpanel.domain.user.repository.UserRepository
+import io.reactivex.Single
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserRepositoryImpl @Inject constructor(
+    private val userDataSource: UserDataSource
+) : UserRepository {
+
+    override fun getUser(username: String): Single<User> {
+        val data = userDataSource.selectUser(username)
+        return data.applyIoScheduler()
+            .doOnError { e->
+                println(e.message)
+            }
+
+    }
+
+}
