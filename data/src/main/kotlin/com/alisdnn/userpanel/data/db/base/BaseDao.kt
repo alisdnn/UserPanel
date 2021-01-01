5ac457a0388f2ef13c99ed9b73d217125d6ca281
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-01 20:30:14 +0330

    Delete fun select and selectAllPaged

--- data/src/main/kotlin/com/alisdnn/userpanel/data/db/base/BaseDao.kt
+++ data/src/main/kotlin/com/alisdnn/userpanel/data/db/base/BaseDao.kt
@@ -1,19 +1,13 @@
 package com.alisdnn.userpanel.data.db.base
 
-import androidx.paging.DataSource
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.IGNORE
 import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Update
-import io.reactivex.Flowable
 import io.reactivex.Single
 
 interface BaseDao<T : Any> {
-
-    fun select(id: Long): Flowable<T>
-
-    fun selectAllPaged(): DataSource.Factory<Int, T>
 
     @Insert(onConflict = IGNORE)
     fun insert(t: T): Long
