c788dae35997bb8ab94ea503add74908612f2853
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-01 20:16:56 +0330

    Add fun select

--- data/src/main/kotlin/com/alisdnn/userpanel/data/db/dao/UserDao.kt
+++ data/src/main/kotlin/com/alisdnn/userpanel/data/db/dao/UserDao.kt
@@ -1,9 +1,14 @@
 package com.alisdnn.userpanel.data.db.dao
 
 import androidx.room.Dao
+import androidx.room.Query
 import com.alisdnn.userpanel.data.db.base.BaseDao
+import io.reactivex.Single
 
 @Dao
 interface UserDao : BaseDao<UserEntity> {
+
+    @Query("SELECT * FROM user WHERE username = :username")
+    fun select(username: String): Single<UserEntity>
 
 }
