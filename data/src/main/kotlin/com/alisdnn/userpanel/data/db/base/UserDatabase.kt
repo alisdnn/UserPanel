e9ecb65c11e67e1c2c88ac90202ebc42ca43067c
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-01 20:37:16 +0330

    Add UserEntity and fun userDao

--- data/src/main/kotlin/com/alisdnn/userpanel/data/db/base/UserDatabase.kt
+++ data/src/main/kotlin/com/alisdnn/userpanel/data/db/base/UserDatabase.kt
@@ -4,14 +4,18 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.alisdnn.userpanel.data.db.Constants.USER_DATABASE_NAME
+import com.alisdnn.userpanel.data.db.dao.UserDao
+import com.alisdnn.userpanel.data.db.dao.UserEntity
 
 @Database(
-    entities = [],
+    entities = [UserEntity::class],
     version = 1,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class UserDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
 
     companion object {
         const val DATABASE_NAME = USER_DATABASE_NAME
