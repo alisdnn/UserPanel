b3065221efd233cc549ec354d781d092e734e08e
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-01 22:14:29 +0330

    Add fun setupViewModel

--- presentation/src/main/kotlin/com/alisdnn/userpanel/presentation/signup/SignupFragment.kt
+++ presentation/src/main/kotlin/com/alisdnn/userpanel/presentation/signup/SignupFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.alisdnn.userpanel.presentation.R
 import com.alisdnn.userpanel.presentation.databinding.FragmentSignupBinding
+import com.alisdnn.userpanel.presentation.extension.observe
 import com.alisdnn.userpanel.presentation.extension.viewBinding
 import com.alisdnn.userpanel.presentation.login.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,12 +21,19 @@ class SignupFragment : Fragment(R.layout.fragment_signup) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupUI()
+        setupViewModel()
     }
 
     private fun setupUI() {
         activity?.title = "Signup"
 
         binding.buttonSignup.setOnClickListener {
+
+        }
+    }
+
+    private fun setupViewModel() {
+        viewModel.run {
 
         }
     }
