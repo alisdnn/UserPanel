bb544a177eec34dbd9db4b86b7613704598eab04
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-01 20:38:14 +0330

    Add fun checkUserValidity

--- presentation/src/main/kotlin/com/alisdnn/userpanel/presentation/login/LoginViewModel.kt
+++ presentation/src/main/kotlin/com/alisdnn/userpanel/presentation/login/LoginViewModel.kt
@@ -1,11 +1,30 @@
 package com.alisdnn.userpanel.presentation.login
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.alisdnn.userpanel.domain.user.usecase.UserLoginParams
+import com.alisdnn.userpanel.domain.user.usecase.UserLoginUseCase
 import com.alisdnn.userpanel.presentation.base.viewmodel.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 class LoginViewModel
 @ViewModelInject
 constructor(
+    private val userLoginUseCase: UserLoginUseCase
 ) : BaseViewModel() {
+
+
+    private val _isUserValid: MutableLiveData<Boolean> = MutableLiveData()
+    val isUserValid: LiveData<Boolean> = _isUserValid
+
+    fun checkUserValidity(username: String, password: String) {
+        userLoginUseCase(UserLoginParams(mapOf("username" to username, "password" to password)))
+            .observeOn(AndroidSchedulers.mainThread())
+            .autoDisposable()
+            .subscribe { item ->
+                _isUserValid.value = item
+            }
+    }
 
 }
