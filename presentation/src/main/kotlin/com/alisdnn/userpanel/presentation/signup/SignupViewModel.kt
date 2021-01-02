a34ddce85092ae3c263fd62a22465501167f307c
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-02 17:56:12 +0330

    Add fun signupUser

--- presentation/src/main/kotlin/com/alisdnn/userpanel/presentation/signup/SignupViewModel.kt
+++ presentation/src/main/kotlin/com/alisdnn/userpanel/presentation/signup/SignupViewModel.kt
@@ -1,9 +1,37 @@
 package com.alisdnn.userpanel.presentation.signup
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.alisdnn.userpanel.domain.user.usecase.UserSignupParams
+import com.alisdnn.userpanel.domain.user.usecase.UserSignupUseCase
 import com.alisdnn.userpanel.presentation.base.viewmodel.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 class SignupViewModel @ViewModelInject constructor(
-): BaseViewModel() {
+    private val userSignupUseCase: UserSignupUseCase
+) : BaseViewModel() {
 
+    private val _isUserAdmin: MutableLiveData<Boolean> = MutableLiveData()
+    val isUserAdmin: LiveData<Boolean> = _isUserAdmin
+
+    fun signupUser(
+        username: String,
+        fullname: String,
+        password: String
+    ) {
+        userSignupUseCase(
+            UserSignupParams(
+                mapOf(
+                    "username" to username,
+                    "fullname" to fullname,
+                    "password" to password
+                )
+            )
+        ).observeOn(AndroidSchedulers.mainThread())
+            .autoDisposable()
+            .subscribe { user ->
+                _isUserAdmin.value = user.isAdmin
+            }
+    }
 }
