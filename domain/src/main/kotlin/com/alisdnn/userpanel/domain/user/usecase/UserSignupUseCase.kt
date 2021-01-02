26bb852a422d4199471baae18d5b325e92e6b423
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-02 17:55:54 +0330

    Add fun invoke impl

--- domain/src/main/kotlin/com/alisdnn/userpanel/domain/user/usecase/UserSignupUseCase.kt
+++ domain/src/main/kotlin/com/alisdnn/userpanel/domain/user/usecase/UserSignupUseCase.kt
@@ -3,6 +3,7 @@ package com.alisdnn.userpanel.domain.user.usecase
 import com.alisdnn.userpanel.domain.base.usecase.SingleUseCase
 import com.alisdnn.userpanel.domain.user.entity.User
 import com.alisdnn.userpanel.domain.user.repository.UserRepository
+import com.alisdnn.userpanel.domain.util.Constants
 import io.reactivex.Single
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -13,6 +14,22 @@ class UserSignupUseCase @Inject constructor(
 ) : SingleUseCase<User, UserSignupParams> {
 
     override fun invoke(params: UserSignupParams): Single<User> {
+
+        val enteredFullname = params.items.getValue("fullname")
+        val enteredUsername = params.items.getValue("username")
+        val enteredPassword = params.items.getValue("password")
+
+        val user = User(
+            isAdmin = enteredUsername == Constants.ADMIN_USERNAME
+                    && enteredPassword == Constants.ADMIN_PASSWORD,
+            username = enteredUsername,
+            fullname = enteredFullname,
+            password = enteredPassword
+        )
+
+        userRepository.insertUser(user)
+
+        return Single.just(user)
 
     }
 
