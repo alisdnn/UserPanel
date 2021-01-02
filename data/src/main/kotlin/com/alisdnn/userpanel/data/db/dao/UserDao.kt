c0121d3a7739fe6acca9b4bd5ab03a483e2041ef
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-02 14:45:44 +0330

    Add fun insertUser

--- data/src/main/kotlin/com/alisdnn/userpanel/data/db/dao/UserDao.kt
+++ data/src/main/kotlin/com/alisdnn/userpanel/data/db/dao/UserDao.kt
@@ -1,6 +1,8 @@
 package com.alisdnn.userpanel.data.db.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.alisdnn.userpanel.data.db.base.BaseDao
 import io.reactivex.Single
@@ -11,4 +13,6 @@ interface UserDao : BaseDao<UserEntity> {
     @Query("SELECT * FROM user WHERE username = :username")
     fun select(username: String): Single<UserEntity>
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    override fun insert(t: UserEntity): Single<Long>
 }
