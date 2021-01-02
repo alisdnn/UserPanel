399205ef57dd272eb63dab35fef82c9fa000ffbb
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-02 15:08:23 +0330

    Add User to UserData mapper

--- data/src/main/kotlin/com/alisdnn/userpanel/data/mapper/EntityToDataExtension.kt
+++ data/src/main/kotlin/com/alisdnn/userpanel/data/mapper/EntityToDataExtension.kt
@@ -1,2 +1,12 @@
 package com.alisdnn.userpanel.data.mapper
 
+import com.alisdnn.userpanel.data.db.dao.UserData
+import com.alisdnn.userpanel.domain.user.entity.User
+
+fun User.map() = UserData(
+    id = id,
+    username = username,
+    email = email,
+    isAdmin = isAdmin,
+    passwordHash = passwordHash
+)
