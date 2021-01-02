f17060ed43c90b31bd3f2a50b0579375b4b2191d
Ali Sadeghian <aliosdn@gmaill.com>
2021-01-02 13:33:41 +0330

    Add SignupViewModel.kt

---
+++ presentation/src/main/kotlin/com/alisdnn/userpanel/presentation/signup/SignupViewModel.kt
@@ -0,0 +1,9 @@
+package com.alisdnn.userpanel.presentation.signup
+
+import androidx.hilt.lifecycle.ViewModelInject
+import com.alisdnn.userpanel.presentation.base.viewmodel.BaseViewModel
+
+class SignupViewModel @ViewModelInject constructor(
+): BaseViewModel() {
+
+}
